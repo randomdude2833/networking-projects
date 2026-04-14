0bb790533429e16709588c7d7014aae3f77e2d94
randomdude2833 <randomdude2833@protonmail.com>
2026-04-14 03:44:32 +0300

    add extensions

--- http-client-and-server/webclient.py
+++ http-client-and-server/webclient.py
@@ -1,25 +1,65 @@
 import socket
 import sys
 import argparse
+import os
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("host")
-parser.add_argument("port", type=int, nargs="?", default=80)
+supported_mime_types = {
+	".txt": "text/plain",
+	".css": "text/css",
+	".html": "text/html",
+	".jpg": "image/jpeg",
+	".jpeg": "image/jpeg",
+	".png": "image/png",
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("host", help="Domain name or IP address.")
+parser.add_argument("port", type=int, nargs="?", default=80, help="Port number. Client uses port 80 as default.")
+parser.add_argument("--method", type=str.upper, choices=["GET", "POST"], default="GET", help="Request method. Supports GET and POST methods only.")
+parser.add_argument("--file", help="File to include as POST payload.")
 
 args = parser.parse_args()
 port = args.port 
 host = args.host
+method = args.method
+file = args.file
+payload = bool(file)
 
 if port < 1 or port > 65535:
 	sys.exit("Port number has to be between 1 and 65535")
 
+if method == "GET" and payload:
+	sys.exit("GET doesn't support sending payload")
+
+if method == "POST" and not payload:
+	sys.exit("POST requires payload (--file FILE)")
+
 request = (
-	"GET / HTTP/1.1\r\n"
+	f"{method} / HTTP/1.1\r\n"
 	f"Host: {host}\r\n"
 	"Connection: close\r\n"
-	"\r\n"
-).encode("ISO-8859-1")
+)
 
+if method == "POST":
+	if not os.path.exists(file):
+		sys.exit(f"File not found: {file}")
+	
+	file_extension = os.path.splitext(file)[1]
+	mime_type = supported_mime_types.get(file_extension, "application/octet-stream")
+	
+	with open(file, "rb") as f:
+		payload = f.read()
+	
+	payload_length = len(payload)
+	request += f"Content-Type: {mime_type}\r\n"
+	request += f"Content-Length: {payload_length}\r\n"
+	request += "\r\n"
+	request = request.encode("ISO-8859-1")
+	request += payload
+else:
+	request += "\r\n"	
+	request = request.encode("ISO-8859-1")
+	
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 client_socket.sendall(request)
@@ -31,8 +71,12 @@ while True:
 		break
 	response_buffer.extend(response_bytes)
 	
-response_parts = response_buffer.split(b"\r\n\r\n")
+response_parts = response_buffer.split(b"\r\n\r\n", 1)
 response_header = response_parts[0].decode("ISO-8859-1")
-response_body = response_parts[1].decode("UTF-8")
+response_body = response_parts[1]
 
+print("===== RESPONSE HEADER =====")
 print(response_header)
+print("===== RESPONSE BODY =====")
+print(response_body)
+
--- http-client-and-server/webserver.py
+++ http-client-and-server/webserver.py
@@ -2,8 +2,8 @@ import socket
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("port", type=int, nargs="?", default=28333)
+parser = argparse.ArgumentParser()
+parser.add_argument("port", type=int, nargs="?", default=28333, help="Port number. Server's default port is 28333.")
 
 args = parser.parse_args()
 port = args.port 
@@ -24,13 +24,16 @@ server_socket.listen()
 
 try:
 	client_socket = None
+	print(f"Server is listening on: 0.0.0.0:{port}")
 
 	while True:
 		client_socket, client_addr = server_socket.accept()
+		print("===== NEW CONNECTION =====")
+		print(f"Client address: {client_addr[0]}:{client_addr[1]}")
 
 		request_buffer = bytearray()
 		while True:
-			request_bytes = client_socket.recv(1024)
+			request_bytes = client_socket.recv(2)
 			if len(request_bytes) == 0:
 				client_socket.close()
 				client_socket = None
@@ -38,6 +41,26 @@ try:
 			
 			request_buffer.extend(request_bytes)
 			if b"\r\n\r\n" in request_buffer:
+				request_header, request_body = request_buffer.split(b"\r\n\r\n", 1)
+				request_headers = request_header.decode("ISO-8859-1").split("\r\n")
+				request_line = request_headers.pop(0)
+
+				method, path, version = request_line.split(" ")
+				print(f"Request method: {method}")
+
+				if method == "POST":
+					content_length = None
+					for header in request_headers:
+						key, value = header.split(":", 1)
+						if key.strip().lower() == "content-length":
+							content_length = int(value.strip())
+
+					if content_length is not None:
+						while content_length - len(request_body) > 0:
+							payload_bytes = client_socket.recv(content_length - len(request_body))
+							request_body += payload_bytes
+						print("Payload:", request_body)
+
 				client_socket.sendall(response)	
 				client_socket.close()
 				client_socket = None
